fcfc5d5fa8f0ed6bcf05cdac537c031d10c70cdc
Vatsal Bajaj <53997634+bajajvatsal@users.noreply.github.com>
2022-11-03 11:48:28 +0530

    app structure added (#5)

---
+++ Dashboard.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import streamlit_authenticator as stauth
+
+st.set_page_config(page_title="Climate Change Impact on Food")
+st.title('Climate Change Impact on Food')
